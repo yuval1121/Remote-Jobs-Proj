875cf5c435f1bc34c3d45e0ee6b2f519517632ca
yuval1121 <yuvalvarshevsky@gmail.com>
2024-06-07 14:09:21 +0300

    added server log

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import fastify from "./app";
 
 fastify.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
-  console.log("Server listening at", address);
+  console.log("Server listening at http://localhost:3000");
   if (err) {
     fastify.log.error(err);
     process.exit(1);
