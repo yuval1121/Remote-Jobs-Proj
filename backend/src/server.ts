d7c22e9b94c442a14b8b687211d1aee5cf237c94
yuval1121 <yuvalvarshevsky@gmail.com>
2024-06-08 19:21:04 +0300

    chore: move early exit to top

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,13 @@
 import fastify from "./app";
 
 fastify.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
-  fastify.swagger();
-
-  console.log("Server listening at http://localhost:3000");
-  console.log("Swagger docs available http://localhost:3000/docs");
-
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
+
+  fastify.swagger();
+
+  console.log("Server listening at http://localhost:3000");
+  console.log("Swagger docs available http://localhost:3000/docs");
 });
